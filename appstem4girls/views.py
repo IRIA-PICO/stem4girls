04413658f18bb0445896d11c8b584a3062e9898c
LeireMiragayaRoca <leiremiragaya@gmail.com>
2025-10-21 13:43:39 +0200

    Views completas

--- appstem4girls/views.py
+++ appstem4girls/views.py
@@ -14,3 +14,32 @@ def index(request):
         'recursos_destacados': recursos_destacados,
     }
     return render(request, 'index.html', context)
+
+def lista_recursos(request):
+    recursos = Recurso.objects.all()
+    return render(request, 'lista_recursos.html', {'recursos': recursos})
+
+
+def detalle_recurso(request, id):
+    recurso = get_object_or_404(Recurso, id=id)
+    return render(request, 'detalle_recurso.html', {'recurso': recurso})
+
+
+def lista_proveedores(request):
+    proveedores = Proveedor.objects.all()
+    return render(request, 'lista_proveedores.html', {'proveedores': proveedores})
+
+
+def detalle_proveedor(request, id):
+    proveedor = get_object_or_404(Proveedor, id=id)
+    return render(request, 'detalle_proveedor.html', {'proveedor': proveedor})
+
+
+def lista_tags(request):
+    tags = Tag.objects.all()
+    return render(request, 'lista_tags.html', {'tags': tags})
+
+
+def detalle_tag(request, id):
+    tag = get_object_or_404(Tag, id=id)
+    return render(request, 'detalle_tag.html', {'tag': tag})

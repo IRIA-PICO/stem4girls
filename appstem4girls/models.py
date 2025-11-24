e24db448291e0aaf505af0aef1654a11425cbce4
Oihane Garcia Alustiza <o.garcia@opendeusto.es>
2025-11-24 21:07:48 +0100

    Intento añadir fotos a recursos

--- appstem4girls/models.py
+++ appstem4girls/models.py
@@ -7,8 +7,7 @@ class Proveedor(models.Model):
     direccion = models.CharField(max_length=200, blank=True)
     telefono = models.CharField(max_length=20, blank=True)
     email = models.EmailField(blank=True)
-    web = models.URLField(blank=True, null=True)
-    #logo = models.ImageField(upload_to='proveedores/', blank=True)
+    web = models.URLField(blank=True, null=True) 
     
     def __str__(self):
         return self.nombre
@@ -29,7 +28,7 @@ class Recurso(models.Model):
     enlace = models.URLField(blank=True)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, related_name='recursos')
     tags = models.ManyToManyField(Tag, related_name='recursos')
-    #imagen = models.ImageField(upload_to='recursos/', blank=True)
+    imagen = models.ImageField(upload_to='img',blank=True,null=True,verbose_name='Image')
 
     def __str__(self):
         return self.titulo

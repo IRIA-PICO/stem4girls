a43764fa75cb104a03a523d65138884bdc01e31e
Iria Pico <iria.pico@opendeusto.es>
2025-11-25 14:45:59 +0100

    urls

--- stem4girls/urls.py
+++ stem4girls/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('appstem4girls/', include('appstem4girls.urls')),
+    path('', include('appstem4girls.urls')),
     path('admin/', admin.site.urls),
 ]

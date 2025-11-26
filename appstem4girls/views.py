9662b45f13dbbbc22c98cfbd8c15ed830560c4dd
Iria Pico <iria.pico@opendeusto.es>
2025-11-26 13:26:11 +0100

    Revert "arreglo"
    This reverts commit 58124939416988e19f83f319f15a31af23837c3d.

--- appstem4girls/views.py
+++ appstem4girls/views.py
@@ -99,7 +99,7 @@ def ajax_search(request):
         results['recursos'].append({
             'id': r.id,
             'titulo': r.titulo,
-            'url': reverse('detalle_recurso', args=[r.id]),
+            'url': reverse('appstem4girls:detalle_recurso', args=[r.id]),
         })
 
     return JsonResponse(results)
--- stem4girls/urls.py
+++ stem4girls/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('appstem4girls.urls')),
+    path('', include('appstem4girls.urls'), namespace='appstem4girls'),
     path('admin/', admin.site.urls),
 ]

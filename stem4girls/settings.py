f9927e2067b361a7cfcd2f2e71bbb28fea705592
Iria Pico <iria.pico@opendeusto.es>
2025-12-01 13:13:49 +0100

    Update settings.py

--- stem4girls/settings.py
+++ stem4girls/settings.py
@@ -134,10 +134,10 @@ LOCALE_PATH = [
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'images')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

8155ca5a8c44b617714298c029e170562dedc58c
LeireMiragayaRoca <leiremiragaya@gmail.com>
2025-12-01 17:04:19 +0100

    LOCALE

--- stem4girls/urls.py
+++ stem4girls/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #path('appstem4girls/', include('appstem4girls.urls')), # LOCALE
     path('', include('appstem4girls.urls')),
     path('admin/', admin.site.urls),
 ]
